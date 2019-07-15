726837665e64ce0147fb196b1ce68130c01045cb
CHAEEUN-LEE <lce4376@gmail.com>
2019-07-15 17:05:34 +0900

    EDIT: styled-component로 만든 컴포넌트를F가 아닌 S로 표시

--- pickin-front/src/utils/Footer/index.tsx
+++ pickin-front/src/utils/Footer/index.tsx
@@ -1,39 +1,39 @@
 import * as React from "react";
-import * as F from "./style";
+import * as S from "./style";
 import { entryLogo, facebookIcon, githubIcon } from "./../../assets/index";
 
 const Footer = () => (
   <div>
-    <F.FooterContainer>
-      <F.FooterContent>
-        <F.LeftContent>
-          <F.EntryLogo src={entryLogo} alt="엔트리" />
-          <F.Copyright>ⓒ 2019 Entry</F.Copyright>
-          <F.FooterInfo>개인정보 처리방침 ㅣ 이용약관</F.FooterInfo>
-          <F.FooterInfo>
+    <S.FooterContainer>
+      <S.FooterContent>
+        <S.LeftContent>
+          <S.EntryLogo src={entryLogo} alt="엔트리" />
+          <S.Copyright>ⓒ 2019 Entry</S.Copyright>
+          <S.FooterInfo>개인정보 처리방침 ㅣ 이용약관</S.FooterInfo>
+          <S.FooterInfo>
             (34111) 대전광역시 유성구 가정북로 76(장동 23-9)
-          </F.FooterInfo>
-          <F.FooterInfo>
+          </S.FooterInfo>
+          <S.FooterInfo>
             교무실: 042-866-8822 / Fax: 042-867-9900
             <span>행정실: 042-866-8885 / Fax: 042-863-4308</span>
-          </F.FooterInfo>
-          <F.FooterInfo>사업자 등록 번호 314-83-01600</F.FooterInfo>
-        </F.LeftContent>
-        <F.RightContent>
-          <F.EntryInfo>Entry 소개</F.EntryInfo>
-          <F.EntryInfoIndex>시스템 소개</F.EntryInfoIndex>
-          <F.EntryInfoIndex>개발자 소개</F.EntryInfoIndex>
+          </S.FooterInfo>
+          <S.FooterInfo>사업자 등록 번호 314-83-01600</S.FooterInfo>
+        </S.LeftContent>
+        <S.RightContent>
+          <S.EntryInfo>Entry 소개</S.EntryInfo>
+          <S.EntryInfoIndex>시스템 소개</S.EntryInfoIndex>
+          <S.EntryInfoIndex>개발자 소개</S.EntryInfoIndex>
           <div>
             <a href="https://www.facebook.com/entrydsm">
-              <F.SNSLogo src={facebookIcon} />
+              <S.SNSLogo src={facebookIcon} />
             </a>
             <a href="https://github.com/EntryDSM">
-              <F.SNSLogo src={githubIcon} />
+              <S.SNSLogo src={githubIcon} />
             </a>
           </div>
-        </F.RightContent>
-      </F.FooterContent>
-    </F.FooterContainer>
+        </S.RightContent>
+      </S.FooterContent>
+    </S.FooterContainer>
   </div>
 );
 
