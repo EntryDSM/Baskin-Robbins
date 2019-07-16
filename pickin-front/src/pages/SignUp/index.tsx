d8e539249b25db26276f6661d3993a12acc4111c
CHAEEUN-LEE <lce4376@gmail.com>
2019-07-16 18:28:51 +0900

    CREATE: SignUp Container

---
+++ pickin-front/src/pages/SignUp/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+import * as S from "./style";
+import { pickinLogo } from "./../../assets/index";
+
+const SignUp = () => (
+  <S.SignUpContainer>
+      <S.SignUpLogo src={pickinLogo} />
+  </S.SignUpContainer>
+);
+
+export default SignUp;
---
+++ pickin-front/src/pages/SignUp/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const SignUpContainer = styled.div`
+  padding: 140px 530px;
+`;
+
+export const SignUpLogo = styled.img`
+  width: 260px;
+  height: 68px;
+`;
