77bfced88f974430d8325c498b0e2057eef02759
CHAEEUN-LEE <lce4376@gmail.com>
2019-07-16 11:14:34 +0900

    [CREATE] landing page

--- pickin-front/src/pages/Landing/index.tsx
+++ pickin-front/src/pages/Landing/index.tsx
@@ -1,9 +1,32 @@
 import * as React from "react";
-import * as L from "./style";
+import { Link } from "react-router-dom";
+
+import * as S from "./style";
 import Footer from "../../utils/Footer";
+import { pickinLogo } from "./../../assets/index";
 
 const Landing = () => (
   <div>
+    <S.LandingBackground>
+      <S.LandingContainer>
+        <Link to="/">
+          <S.LandingLogo src={pickinLogo} />
+        </Link>
+        <S.LandingText>
+          <S.LandingMainText>
+            Hello <br /> This is Interview System
+          </S.LandingMainText>
+          <S.LandingSubText>Please sign up or sign in</S.LandingSubText>
+          <Link to="/signin">
+            <S.SignInButton>Sign in</S.SignInButton>
+          </Link>
+          <S.SignInOrUp>or</S.SignInOrUp>
+          <Link to="/signup">
+            <S.SignUpLink>Sign up</S.SignUpLink>
+          </Link>
+        </S.LandingText>
+      </S.LandingContainer>
+    </S.LandingBackground>
     <Footer />
   </div>
 );
