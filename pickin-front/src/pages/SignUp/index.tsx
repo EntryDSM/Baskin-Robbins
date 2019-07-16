c8b4a363301538b9813f5ac41249d558fee2ac71
CHAEEUN-LEE <lce4376@gmail.com>
2019-07-16 20:05:14 +0900

    CREATE: sign up page

--- pickin-front/src/pages/SignUp/index.tsx
+++ pickin-front/src/pages/SignUp/index.tsx
@@ -1,11 +1,28 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
 
 import * as S from "./style";
-import { pickinLogo } from "./../../assets/index";
+import { pickinLogo, interviewerIcon, agencyIcon } from "./../../assets/index";
 
 const SignUp = () => (
   <S.SignUpContainer>
-      <S.SignUpLogo src={pickinLogo} />
+    <S.SignUpLogo src={pickinLogo} />
+    <S.SignUpTypes>
+      <Link to="/signup/interviewer">
+        <S.SignUpTypeBtn>
+          <S.SignUpTypeIcon src={interviewerIcon} />
+          <p>면접관 가입</p>
+          <p>면접관으로 가입하시려면 선택해 주세요.</p>
+        </S.SignUpTypeBtn>
+      </Link>
+      <Link to="/signup/agency">
+        <S.SignUpTypeBtn>
+          <S.SignUpTypeIcon src={agencyIcon} />
+          <p>기관 가입</p>
+          <p>기관으로 가입하시려면 선택해 주세요.</p>
+        </S.SignUpTypeBtn>
+      </Link>
+    </S.SignUpTypes>
   </S.SignUpContainer>
 );
 
