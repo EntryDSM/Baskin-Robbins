13560d39f199975f2fab5e3b2b6c63fb0fdb7985
CHAEEUN-LEE <lce4376@gmail.com>
2019-09-05 20:11:33 +0900

    UPDATE: input field의 바뀐 값을 state에 저장

--- pickin-front/src/pages/SignUp/InterviewerSignUp/index.tsx
+++ pickin-front/src/pages/SignUp/InterviewerSignUp/index.tsx
@@ -4,22 +4,50 @@ import { Link } from "react-router-dom";
 import * as S from "./../style";
 import { pickinLogo } from "./../../../assets";
 import { ClauseAdmit, InputBox } from "./../../../component";
+import { checkAgencyCode } from "./../../../services/get";
+import { signUpToInterviewer } from "./../../../services/post";
 
 interface Props {}
 
 interface State {
   isChecked: boolean;
+  agencyCode: string;
+  name: string;
+  email: string;
+  password: string;
+  passwordConfirm: string;
 }
 
 class AgencySignUp extends React.Component<Props, State> {
   state = {
-    isChecked: false
+    isChecked: false,
+    agencyCode: "",
+    name: "",
+    email: "",
+    password: "",
+    passwordConfirm: ""
   };
 
-  public setChecked = isChecked =>
+  private setChecked = isChecked =>
     this.setState({
       isChecked: !isChecked
     });
+
+  private handleAgencyCode = (e: React.ChangeEvent<HTMLInputElement>): void =>
+    this.setState({ agencyCode: e.target.value });
+
+  private handleName = (e: React.ChangeEvent<HTMLInputElement>): void =>
+    this.setState({ name: e.target.value });
+
+  private handleEmail = (e: React.ChangeEvent<HTMLInputElement>): void =>
+    this.setState({ email: e.target.value });
+
+  private handlePassword = (e: React.ChangeEvent<HTMLInputElement>): void =>
+    this.setState({ password: e.target.value });
+
+  private handlePasswordConfirm = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ): void => this.setState({ passwordConfirm: e.target.value });
 
   render() {
     return (
@@ -27,11 +55,34 @@ class AgencySignUp extends React.Component<Props, State> {
         <Link to="/">
           <S.SignUpFormLogo src={pickinLogo} alt="피킨 로고" />
         </Link>
-        <InputBox boxType="기관 코드" btnType="확인" />
-        <S.SignUpInputBox placeholder="이름" />
-        <InputBox boxType="이메일" btnType="인증" />
-        <S.SignUpInputBox placeholder="비밀번호" type="password" />
-        <S.SignUpInputBox placeholder="비밀번호 확인" type="password" />
+        <InputBox
+          boxType="기관 코드"
+          value={this.state.agencyCode}
+          changeState={this.handleAgencyCode}
+          btnType="확인"
+        />
+        <S.SignUpInputBox
+          placeholder="이름"
+          value={this.state.name}
+          onChange={this.handleName}
+        />
+        <InputBox
+          boxType="이메일"
+          value={this.state.email}
+          changeState={this.handleEmail}
+          btnType="인증"
+        />
+        <S.SignUpInputBox
+          placeholder="비밀번호"
+          type="password"
+          value={this.state.password}
+          onChange={this.handlePassword}
+        />
+        <S.SignUpInputBox
+          placeholder="비밀번호 확인"
+          type="password"
+          onChange={this.handlePasswordConfirm}
+        />
         <ClauseAdmit
           isChecked={this.state.isChecked}
           setChecked={this.setChecked}
