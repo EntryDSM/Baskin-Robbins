ce99f56ea942130dfee44291037faaa1c9d069d3
CHAEEUN-LEE <lce4376@gmail.com>
2019-09-05 13:58:45 +0900

    MODIFY: 오타 제거

--- pickin-front/src/pages/index.ts
+++ pickin-front/src/pages/index.ts
@@ -3,4 +3,4 @@ export { default as SignUp } from "./SignUp";
 export { default as AgencySignUp } from "./SignUp/AgencySignUp";
 export { default as InterviewerSignUp } from "./SignUp/InterviewerSignUp";
 export { default as Main } from "./Main"; 
-export { default as SignIn } from "./SignIn";]
+export { default as SignIn } from "./SignIn";
