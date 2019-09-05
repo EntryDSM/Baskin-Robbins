8b6983e487b16c0d57245d6c0ddc74f26bad60e2
CHAEEUN-LEE <lce4376@gmail.com>
2019-09-05 13:58:24 +0900

    CREATE: email error component

---
+++ pickin-front/src/component/SignUp/emailerror/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+import * as S from "./../style";
+
+const EmailError: React.FC = () => <div />;
+
+export default EmailError;
--- pickin-front/src/component/index.ts
+++ pickin-front/src/component/index.ts
@@ -1,2 +1,4 @@
 export { default as ClauseAdmit } from "./SignUp/clauseadmit";
 export { default as InputBox } from "./SignUp/inputbox";
+export { default as EmailError } from "./SignUp/emailerror";
+export { default as InputError } from "./SignUp/inputerror";
